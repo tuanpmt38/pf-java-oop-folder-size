90544472ead9371fff8fc1b249527aaa84031774
tuanpmt38 <37221796+tuanpmt38@users.noreply.github.com>
2018-04-18 23:34:46 +0700

    pf-java-oop size folder

---
+++ FileSize.java
@@ -0,0 +1,25 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class FileSize {
+    public static long getFile(File file) {
+        long len = 0;
+        File[] list = file.listFiles();
+        if (list == null || list.length == 0) {
+            return len;
+        }
+        for (File f : list) {
+            if (f.isFile())
+                len += f.length();
+            else
+                len += getFile(f);
+        }
+        return len;
+    }
+
+    public static void main(String[] args) {
+        File folder = new File("D:\\codegym\\Exercise-java\\oop\\pf-java-oop-get-size-folder\\src");
+        long size = getFile(folder);
+        System.out.println("folder in size: " + size);
+    }
+}
